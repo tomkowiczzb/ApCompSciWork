dad06a1bfcce6fc1c5357eecceacf53323e7caa5
ztomk <ztomk@10.0.0.253>
2021-02-05 13:58:55 -0700

    Initial Creation

---
+++ pendas/src/CircleRunner.java
@@ -0,0 +1,45 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+import java.util.*;
+
+public class CircleRunner
+{
+	public static void main( String[] args )
+	{
+		Scanner kb = new Scanner( System.in );
+		System.out.print( "Enter the radius ::" );
+		double radius = kb.nextDouble();
+		
+		//print your answer here
+		
+		System.out.println( "Area = " + (3.14159*(radius*radius)));
+	}
+}
+
+/*
+ 
+Sample Data: 
+7.5
+10
+72.534
+55
+101
+99.952
+
+
+
+
+Sample Output : 
+Circle area is :: 176.71
+Circle area is :: 314.16
+Circle area is :: 16528.49
+Circle area is :: 9503.32
+Circle area is :: 32047.39
+Circle area is :: 31385.77
+Circle area is :: 31415.93
+
+
+
+
+*/
---
+++ pendas/src/Divide.java
@@ -0,0 +1,28 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+
+//division example
+
+import static java.lang.System.*;
+
+public class Divide
+{
+   public static void main( String args[] )
+   {
+		out.println( 1 / 2  );
+		out.println( 1 / 2.0 );
+		out.println( 1.0 / 2  );
+		out.println( 1.0 / 2.0 );	
+			
+		//out.println( 1 / 0  );	//division by zero
+		out.println( 0 / 1 );							
+
+		out.println(2 / 3);
+
+		out.println(3 / 2);
+
+		out.println(9 / 3);
+
+		out.println(9.2 / 3);
+   }
+}
